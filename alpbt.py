6e08536e7746fa604dead3e9db97b311e72fe715
HU Xinqiao <huxinq@gmail.com>
2020-11-06 22:59:17 +0800

    restructure

--- alpbt.py
+++ alpbt.py
@@ -3,51 +3,63 @@ import collections
 import math
 
 class AlphabeticTree:
-    def __init__(self):
-        self.root = node.Node()
-        self.count = collections.Counter()
-        self.depth = dict()
+    def __init__(self, size = 1024):
+        self.count = collections.Counter({k:1 for k in list(range(1, size+1))})
+        self.depth = AlphabeticTree.find_all_required_depth(self.count)
+        self.root = node.Node.rebuild(AlphabeticTree.half_kraft(self.depth))
+        self.size = size
 
     def access(self, data):
-        # check if a new data is being accessed
-        # if so, create keys for it in the dictionaries
-        if (not data in self.count):
-            self.count[data] = 0
-            self.depth[data] = None
+        if (data < self.size + 1 and data > 0):
+            self.count[data] += 1
+            max_depth = AlphabeticTree.find_required_depth(self.count, data)
+            if (max_depth < self.depth[data]):
+                self.move_up(data, self.depth[data] - max_depth)
 
-        # calculate new count
-        self.count[data] += 1
+    def move_up(self, data, depth_decrease):
+        # first, check if the node can be relocated by reducing solitary edges
+        node_in_question = self.root.find_node(data)
+        reduced_amount = 0
+        all_required_depth = AlphabeticTree.find_all_required_depth(self.count)
+        if (node_in_question.has_parent()):
+            reduced_amount = node_in_question.reduce_edge_length_by(depth_decrease)
+            self.depth[data] = self.depth[data] - reduced_amount
+            
+        if (reduced_amount < depth_decrease):
+            rebuild_root = node_in_question.find_ancestor(depth_decrease - reduced_amount+1)
+            while sum([2**(- d + rebuild_root.find_depth()) for data, d in all_required_depth.items() if data in range(rebuild_root.min, rebuild_root.max+1)]) > 1/2:
+                    rebuild_root = rebuild_root.find_parent()
+                    
+            rebuild_depth = {data : depth - rebuild_root.find_depth() for data, depth in all_required_depth.items() if data in range(rebuild_root.min, rebuild_root.max+1)}
+            new_sub_tree = node.Node.rebuild(AlphabeticTree.half_kraft(rebuild_depth))
+            if rebuild_root.has_parent is not True:
+                self.root = new_sub_tree
+            else:
+                rebuild_root.replace(new_sub_tree)
+            for data, value in new_sub_tree.find_all_depth().items():
+                self.depth[data] = value + new_sub_tree.find_depth()
 
-        # calculate new depth dictated
-        depth_req = AlphabeticTree.half_kraft(self.count)
-        violating_nodes = AlphabeticTree.find_violating_nodes(self.depth, depth_req)
-        # if there are violating nodes
-        # rebuild according to the depth req
-        if violating_nodes:
-            self.root.half_kraft_rebuild(depth_req)
-
-    
     @staticmethod
-    def half_kraft(count):
-        total = sum(count.values())
-        depth_req = dict()
-        for key in count:
-            depth_req[key] = math.ceil(math.log2(total/count[key]))+1
-        return depth_req
+    def find_required_depth(count, data):
+        return math.ceil(math.log2(sum(count.values())/count[data]))+1
 
     @staticmethod
-    def find_violating_nodes(real, req):
-        ret = []
-        for key, value in real.items():
-            if value is None or value > req[key]:
-                ret += key
-        return ret
+    def find_all_required_depth(count):
+        num = math.log2(sum(count.values()))
+        return {d : math.ceil(num/count[d])+1 for d in count.keys()}
 
-    # returns an ordered dictionary of encodings based on depths
     @staticmethod
-    def half_kraft_encode(dict):
-        sorted_dict = collections.OrderedDict(sorted(dict))
-        ret = dict()
-        first_key, first_val = sorted_dict.popitem(False)
+    def half_kraft(depth):
+        sorted_depth = collections.OrderedDict(sorted(depth.items()))
+        ret = {}
+        first_key, first_val = sorted_depth.popitem(False)
         ret[first_key] = [True] * first_val
-        remaining_list = list(sorted_dict.items())
+        previous_location = ret[first_key]
+
+        while (sorted_depth):
+            key, val = sorted_depth.popitem(False)
+            k = max([w for w in range(1,min(len(previous_location), val)+1) if previous_location[w-1] is True])
+            ret[key] = previous_location[0:k-1] + [False] + [True] * (val-k)
+            previous_location = ret[key]
+        
+        return ret
--- node.py
+++ node.py
@@ -1,8 +1,9 @@
 import collections
 
 class Node:
-    def __init__(self, left=None, right=None):
-        self.left, self.right, self.max, self.min = left, right, None, None
+    def __init__(self, left=None, left_length=0, right=None, right_length=0, parent=None):
+        self.left, self.right, self.max, self.min, self.parent = left, right, None, None, parent
+        self.left_length, self.right_length = left_length, right_length
 
     def are_leaves_leafnodes(self):
         return (self.left is None or self.left.are_leaves_leafnodes()) and \
@@ -17,36 +18,111 @@ class Node:
             list = list + self.right.in_order_traversal()
         return list
 
-    def insert_left(self, node):
-        self.left = node
-        self.max = max(self.max, self.left.max) if \
-            (self.left.max is not None and self.max is not None) else \
-                (self.max if self.max is not None else self.left.max)
-        self.min = min(self.min, self.left.min) if \
-            (self.left.min is not None and self.min is not None) else \
-                (self.min if self.min is not None else self.left.min)
+    def insert(self, node, left):
+        if left is True:
+            self.left = node
+            self.left_length = 1
+        else:
+            self.right = node
+            self.right_length = 1
 
-    def insert_right(self, node):
-        self.right = node
-        self.max = max(self.max, self.right.max) if \
-            (self.right.max is not None and self.max is not None) else \
-                (self.max if self.max is not None else self.right.max)
-        self.min = min(self.min, self.right.min) if \
-            (self.right.min is not None and self.min is not None) else \
-                (self.min if self.min is not None else self.right.min)
+        node.parent = self
+        child = node
+        parent = self
+        while parent is not None:
+            parent.max = max(parent.max, child.max) if \
+            (child.max is not None and parent.max is not None) else \
+                (parent.max if parent.max is not None else child.max)
+            parent.min = min(parent.min, child.min) if \
+            (child.min is not None and parent.min is not None) else \
+                (parent.min if parent.min is not None else child.min)
+            child = parent
+            parent = parent.parent
+
+        return node
     
     def encode(self, data):
         if data > self.max or data < self.min:
             print(repr(data) + "doesn't exist in the tree")
             return ""
-        if self.left is not None and self.left.max is not None and self.left.max >= data:
-            return "0" + self.left.encode(data)
-        if self.right is not None and self.right.min is not None and self.right.min <= data:
-            return "1" + self.right.encode(data)
+        else:    
+            if self.left is not None and self.left.max is not None and self.left.max >= data:
+                return "0" + self.left.encode(data)
+            if self.right is not None and self.right.min is not None and self.right.min <= data:
+                return "1" + self.right.encode(data)
 
-    def half_kraft_rebuild(self, dict):
-        NotImplemented
+    def find_node(self, data):
+        if data > self.max or data < self.min:
+            print(repr(data) + " doesn't exist in the tree")
+            return None
+        else:
+            if self.left is not None and self.left.max is not None and self.left.max >= data:
+                return self.left.find_node(data)
+            if self.right is not None and self.right.min is not None and self.right.min <= data:
+                return self.right.find_node(data)
+    
+    def find_all_depth(self):
+        ret = {}
+        if self.left is not None:
+            ret.update({data : key+1 for data, key in self.left.find_all_depth().items()})
+        if self.right is not None:
+            ret.update({data : key+1 for data, key in self.right.find_all_depth().items()})
+        return ret
 
+    def find_depth(self):
+        if self.has_parent():
+            return self.parent.find_depth()+1
+        else:
+            return 0
+
+    def has_parent(self):
+        return self.parent is not None
+    
+    def has_child(self, left):
+        if left:
+            return self.left is not None
+        else:
+            return self.right is not None
+    
+    def replace(self, node):
+        if self.has_parent():
+            self.parent.left.insert(node, self.parent.left is self)
+
+    def reduce_edge_length_by(self, reduction):
+        ret = 0
+        if self.parent.left is self:
+            ret = min(self.parent.left_length-1, reduction)
+            self.parent.left_length -= ret
+        else:
+            ret = min(self.parent.right_length-1, reduction)
+            self.parent.right_length -= ret
+        return ret
+
+    def find_ancestor(self, generation):
+        cur = self
+        for _ in range(generation):
+            cur = cur.parent
+        return cur
+
+    def find_parent(self):
+        if self.parent is not None:
+            return self.parent
+        else:
+            print("no parent found")
+
+
+    @staticmethod
+    def rebuild(location):
+        root = Node()
+        for data, direction in location.items():
+            cur = root
+            for is_left in direction[:-1]:
+                if cur.has_child(is_left):
+                    cur = cur.left if is_left else cur.right
+                else:
+                    cur = cur.insert(Node(), is_left)
+            cur.insert(LeafNode(data), direction[-1])
+        return root
 
 class LeafNodeChildrenError(Exception):
     def __init__(self, data):
@@ -66,11 +142,15 @@ class LeafNode(Node):
     def are_leaves_leafnodes(self):
         return True
 
-    def insert_left(self, node):
-        raise(LeafNodeChildrenError(self.data))
-
-    def insert_right(self, node):
-        raise(LeafNodeChildrenError(self.data))
-
     def encode(self, data):
         return ""
+
+    def find_all_depth(self):
+        return {self.data : 0}
+        
+    def find_node(self, data):
+        if data is self.data:
+            return self
+        else:
+            print("node not found")
+            return None
